ece08612f54ee7be9d26a71ae715e56b247f3c67
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-09 13:19:44 -0700

    Making packages actually run and test and then also flow

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import appendM from "./"
+import appendM from "./source.js"
 
 test(({same, end}) => {
   same(appendM("a")([]), ["a"])
