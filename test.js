240a940d4a2a2a072bfbb46563eb905e3ca6185d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:17 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import appendM from "./source.js"
+import append from "./source"
 
 test(({same, end}) => {
   same(appendM("a")([]), ["a"])
