45f77129ba387642c22e5669785e8120311fbf0a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:17 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import append from "./source"
+import appendM from "./source"
 
 test(({same, end}) => {
   same(appendM("a")([]), ["a"])
