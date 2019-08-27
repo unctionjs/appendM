dadaa1d4cbe8f085f68feb4bc93375da90f960c6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-08-27 02:06:17 -0700

    Switching to typescript

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-/* eslint-disable flowtype/require-return-type */
+
 import {test} from "tap";
 
-import appendM from "./index";
+import appendM from "./";
 
 test(({same, end}) => {
   same(appendM("a")([]), ["a"]);
